20f6abe1628024a85513bb9304525036161d2261
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2020-01-21 16:38:13 +0500

    First commit

---
+++ Nunit.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NunitFrameworkBasic
+{
+    public  class Nunit
+    {
+       IWebDriver driver;
+
+        [SetUp]
+        public  void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public  void OpenWebsite()
+        {    
+            driver.Navigate().GoToUrl("https://demo.nopcommerce.com");           
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+
+    }
+}
